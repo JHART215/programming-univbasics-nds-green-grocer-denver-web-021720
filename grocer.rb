5fc638a598e57cbcfb592de459c10ba88ec3e5d9
Jeremy Hart <jerome215@comcast.net>
2020-02-08 20:58:50 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,9 +39,9 @@ def apply_coupons(cart, coupons)
       cart_item_with_coupon[:count] += coupons[counter][:num]
     else
       cart_item_with_coupon = {
-        :item => couponed_item_name
-        :price => coupons[counter][:cost] / coupons[counter][:num]
-        :count => coupons[counter][:num]
+        :item => couponed_item_name,
+        :price => coupons[counter][:cost] / coupons[counter][:num],
+        :count => coupons[counter][:num],
         :clearance => cart_item[:clearance]
       }
       cart << cart_item_with_coupon
