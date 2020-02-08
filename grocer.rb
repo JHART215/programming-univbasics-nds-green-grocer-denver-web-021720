c3e52b4275cafe8a41a38eb355fd7331333e642c
Jeremy Hart <jerome215@comcast.net>
2020-02-08 22:01:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,7 +40,10 @@ def apply_coupons(cart, coupons)
         cart_item_with_coupon[:count] += coupons[counter][:num]
         cart_item[:count] -= coupons[counter][:num]
       else
-        
+        cart_item_with_coupon = {
+          :item = couponed_item_name,
+          :price = 
+        }
       end    
     end
     counter += 1
