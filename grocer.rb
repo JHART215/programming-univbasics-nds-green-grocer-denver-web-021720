4c98173ef258473719579434ca2597e7044b1600
Jeremy Hart <jerome215@comcast.net>
2020-02-08 20:32:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -32,9 +32,9 @@ end
 def apply_coupons(cart, coupons)
    counter = 0
    while counter < coupons.length
-    cart_item = find_item_by_name_in_collection{coupons[counter][:item], cart)
+    cart_item = find_item_by_name_in_collection(coupons[counter][:item], cart)
     couponed_item_name = "#{coupon[counter][:item]} W/ Coupon"
-    cart_item_with_coupon = 
+    cart_item_with_coupon = find_item_by_name_in_collection(coupons[counter])
 end
 
 def apply_clearance(cart)
