a707c868fcdd1f030598a43ed360023cf3800c5c
Jeremy Hart <jerome215@comcast.net>
2020-02-08 20:35:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -40,7 +40,7 @@ def apply_coupons(cart, coupons)
     else
       cart_item_with_coupon = {
         :item => couponed_item_name
-        :price => 
+        :price => coupons[]
         :count => 
         :clearance => 
       }
