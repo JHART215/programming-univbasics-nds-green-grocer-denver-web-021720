6629867ea26a0d77c1a899bb1b8ac6500020293c
Jeremy Hart <jerome215@comcast.net>
2020-02-08 20:33:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -37,6 +37,8 @@ def apply_coupons(cart, coupons)
     cart_item_with_coupon = find_item_by_name_in_collection(couponed_item_name, cart)
     if cart_item_with_coupon
       cart_item_with_coupon[:count] += coupons[counter][:num]
+    else
+      
 end
 
 def apply_clearance(cart)
