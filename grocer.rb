a1b598395cc79f4e3c2d751ef36614c20c7db441
Jeremy Hart <jerome215@comcast.net>
2020-02-08 20:18:43 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ def consolidate_cart(cart:[])
       new_cart_item = {
         :item => cart[counter][:item]
         :price => cart[counter][:price]
-        :clearance => 
+        :clearance => cart[counter][:clearance]
         :count => 1
       }
 end
