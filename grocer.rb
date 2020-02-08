e63aa20790ea9c3aec5205c34e41fafa70404008
Jeremy Hart <jerome215@comcast.net>
2020-02-08 20:17:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,7 +16,7 @@ def consolidate_cart(cart:[])
     if new_cart_item != NIL
       new_cart_item[:count] += 1
     else
-      
+      new_cart_item = []
 end
 
 def apply_coupons(cart:[], coupons:[])
