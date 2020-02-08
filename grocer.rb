3859729a3884eddd03ae1cc34c20d201cdebaaf9
Jeremy Hart <jerome215@comcast.net>
2020-02-08 20:37:08 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -45,7 +45,8 @@ def apply_coupons(cart, coupons)
         :clearance => cart_item[:clearance]
       }
       cart << cart_item_with_coupon
-      cart_item[:count] -= coupons[counter]
+      cart_item[:count] -= coupons[counter][:num]
+      
 end
 
 def apply_clearance(cart)
